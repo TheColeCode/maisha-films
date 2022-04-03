c9993e1a3d2a6bf8ba3d1cecf6d66992bb63f08e
Cole Code <thecolecode@gmail.com>
2022-04-03 08:39:38 -0700

    add-card: add card styles for films

---
+++ components/Card.js
@@ -0,0 +1,33 @@
+import ArrowIcon from '../components/ArrowIcon';
+
+export default function Card({title, releaseDate, about, trailer, poster, links}) {
+  return (
+    <li
+      className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0 p-4 m-2"
+    >
+      <div style={{'display': 'grid', 'grid-template-columns': '200px auto'}}>
+        <div className="mr-4">
+          <p>{poster}</p>
+        </div>
+        <div>
+          <h3 className="uppercase mb-3 font-bold">{title}</h3>
+          <p className="uppercase mb-3 font-bold opacity-60">Release Date: {releaseDate}</p>
+          <div className="flex">
+            <a href={trailer} title="Finding Hope Home Trailer" target="_blank" rel="noreferrer" className="font-bold" style={{'color': 'white'}}>Trailer</a>
+            <ArrowIcon className="mt-0.5" />
+          </div>
+          <div className="mt-2 mb-2">
+            <>{about}</>
+          </div>
+        </div>
+      </div>
+      <ul style={{'display': 'flex', 'justify-content': 'space-between', 'width': '100%', 'max-width': '400px', 'margin': '0 auto'}}>
+        <li><a href={links.imdb} target="_blank" rel="noreferrer" className="" style={{'font-size': '10pt', 'color': 'white'}}>IMdb</a></li>
+        <li><a href={links.facebook} target="_blank" rel="noreferrer" style={{'font-size': '10pt', 'color': 'white'}}>Facebook</a></li>
+        <li><a href={links.youtube} target="_blank" rel="noreferrer" style={{'font-size': '10pt', 'color': 'white'}}>Youtube</a></li>
+        <li><a href={links.soundtrack} target="_blank" rel="noreferrer" style={{'font-size': '10pt', 'color': 'white'}}>Soundtrack</a></li>
+      </ul>
+    </li>
+  );
+}
+  
--- pages/index.js
+++ pages/index.js
@@ -3,61 +3,36 @@ import { getPosts } from '../utils/mdx-utils';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Card from '../components/Card';
 import Layout, { GradientBackground } from '../components/Layout';
-import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
+import { films } from '../utils/films';
 
-export default function Index({ posts, globalData }) {
+export default function Index({ globalData }) {
+
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
       <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
+        <h1 className="text-1xl lg:text-3xl font-bold m-2 mb-6">
           {globalData.blogTitle}
         </h1>
-        <div>
-          <h3 className="text-3xl lg:text-2xl text-center">Official Trailer</h3>
-          <iframe
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/0OcizQfU6Gk"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            style={{'margin': '0 auto', 'max-width': '560px', 'min-height': '315px'}}
-          />
-        </div>
-        {/* <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
+
+        <ul className="w-full">
+          {films.map((film) => (
+            <Card
+              key={film.title}
+              title={film.title}
+              releaseDate={film.releaseDate}
+              about={film.about}
+              trailer={film.trailer}
+              poster={film.poster}
+              links={film.links}
+            />
           ))}
-        </ul> */}
+        </ul>
       </main>
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground
