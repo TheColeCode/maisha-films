79838090373980aee62c849b8f44d1a268f3e2bc
Cole Code <thecolecode@gmail.com>
2022-04-02 22:22:15 -0700

    customize-landing: customize landing page with branding

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,19 @@
 import Link from 'next/link';
+import Logo from 'next/image';
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+    <header className="pt-20">
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
-          <a>{name}</a>
+        <Link href="/"> 
+          <a><Logo src='/Maisha-Films-Logo.png' alt="Maisha Films Logo" title="Maisha Films - Home" width="160px" height="160px" /></a>
         </Link>
       </p>
+      {/* <nav>
+      <Link href="/"> 
+        <a>IMdb</a>
+      </Link>
+      </nav> */}
     </header>
   );
 }
